f4172179aa810333d9eb709c571eccd28136d21a
atenobi <atenobimailru@gmail.com>
2022-07-07 18:15:44 +0200

    deploy ready dev 11

--- src/api/getGamesByName.js
+++ src/api/getGamesByName.js
@@ -1,22 +1,42 @@
+const axios = require("axios");
+
 export const getGamesByName = async (name) => {
-  let result = [];
+  // let result = [];
 
-  await fetch("https://api.igdb.com/v4/games/", {
-    method: "POST",
-    mode: "no-cors",
-    headers: {
-      "access-control-allow-origin" : "*",
-      "Client-ID": "r8ndcz4yox3p6e5ndgzrhlmbsharhk",
-      authorization: "Bearer 19bj2thdtj93gj9en7lccpcz4hirsu",
-    },
-    body: `fields id, name, release_dates.human, rating, age_ratings.rating, game_engines.name, summary, cover.url, genres.name, platforms.name, game_modes.name, url;
-        search "${name}";
-        limit 500;`,
-  })
-    .then((response) => response.json())
-    .then((data) => (result = data));
+  const queryParams = {
+    
+      method: "POST",
+      
+      headers: {
+        
+        "Client-ID": "r8ndcz4yox3p6e5ndgzrhlmbsharhk",
+        authorization: "Bearer 19bj2thdtj93gj9en7lccpcz4hirsu",
+      },
+      body: `fields id, name, release_dates.human, rating, age_ratings.rating, game_engines.name, summary, cover.url, genres.name, platforms.name, game_modes.name, url;
+          search "${name}";
+          limit 500;`,
+    
+  }
 
-  return result;
+  
+
+const options = {
+  method: 'GET',
+  url: 'https://scrapers-proxy2.p.rapidapi.com/standard',
+  params: {url: `https://api.igdb.com/v4/games/${JSON.stringify(queryParams)}`},
+  headers: {
+    'X-RapidAPI-Key': '37cc2137c4mshe381a547fd72629p1dcd8cjsn19fd759ff090',
+    'X-RapidAPI-Host': 'scrapers-proxy2.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+
+  // return result;
 };
 
 // const axios = require("axios");
@@ -24,7 +44,7 @@ export const getGamesByName = async (name) => {
 // const options = {
 //   method: 'GET',
 //   url: 'https://scrapers-proxy2.p.rapidapi.com/standard',
-//   params: {url: 'https://example.com'},
+//   params: {url: 'https://api.igdb.com/v4/games/'},
 //   headers: {
 //     'X-RapidAPI-Key': '37cc2137c4mshe381a547fd72629p1dcd8cjsn19fd759ff090',
 //     'X-RapidAPI-Host': 'scrapers-proxy2.p.rapidapi.com'

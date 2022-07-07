7515e19e4a3164c3db6043dd62bb6c20937826e9
atenobi <atenobimailru@gmail.com>
2022-07-07 16:40:40 +0200

    deploy ready dev 6

--- src/api/getGamesByName.js
+++ src/api/getGamesByName.js
@@ -16,3 +16,21 @@ export const getGamesByName = async (name) => {
 
   return result;
 };
+
+// const axios = require("axios");
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://scrapers-proxy2.p.rapidapi.com/standard',
+//   params: {url: 'https://example.com'},
+//   headers: {
+//     'X-RapidAPI-Key': '37cc2137c4mshe381a547fd72629p1dcd8cjsn19fd759ff090',
+//     'X-RapidAPI-Host': 'scrapers-proxy2.p.rapidapi.com'
+//   }
+// };
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });

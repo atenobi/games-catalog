b2d94f7d480595ded3f39665b656952239acc59e
atenobi <atenobimailru@gmail.com>
2022-07-09 20:56:50 +0200

    deploy ready dev 20

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import * as path from 'path'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,9 +18,18 @@ export default defineConfig({
   },
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
   build: {
-outDir: "./docs"
+    outDir: "./docs",
+    sourcemap: true,
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      }
+    }
   },
 });

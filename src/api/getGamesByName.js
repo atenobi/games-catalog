2a32c8b4365e568f28f3150f755e593fdaecac95
atenobi <atenobimailru@gmail.com>
2022-07-10 19:13:49 +0200

    try new script prod 7

--- src/api/getGamesByName.js
+++ src/api/getGamesByName.js
@@ -1,14 +1,12 @@
 export const getGamesByName = async (name) => {
   let result = [];
 
-  const queryParams = {
-    fields: 'id,name,release_dates.human,rating,age_ratings.rating,game_engines.name,summary,cover.url,genres.name,platforms.name,game_modes.name,url',
-    search: `${name}`,
-    sort: "rating desc",
-    limit: '500',
-  }
-
-  await fetch(`https://eozcxj7i2j3mjjh.m.pipedream.net/games${queryParams}`, {
+  const queryParams = new URLSearchParams();
+  queryParams.set(fields, 'id,name,release_dates.human,rating,age_ratings.rating,game_engines.name,summary,cover.url,genres.name,platforms.name,game_modes.name,url');
+  queryParams.set(search, `${name}`);
+  queryParams.set(limit, 500);
+  
+  await fetch(`https://eozcxj7i2j3mjjh.m.pipedream.net/games${queryParams.toString()}`, {
     method: "POST",
     headers: {
       "Client-ID": "r8ndcz4yox3p6e5ndgzrhlmbsharhk",
@@ -20,3 +18,15 @@ export const getGamesByName = async (name) => {
 
   return result;
 };
+
+// const saveParamsToUrl = (nameParam, valueParam, addressApi) => {
+//   let myResultUrl = '';
+//   const URlUserParams = new URLSearchParams();
+
+//   URlUserParams.append(nameParam, valueParam);
+
+//   myResultUrl = addressApi + URlUserParams.toString();
+//   return myResultUrl;
+// };
+
+// export default saveParamsToUrl;
